36abe29211f0aa25f256e1e404368a3320e21140
davidfang <mw_fang@163.com>
2016-07-21 22:44:52 +0800

    测试可以了

---
+++ src/components/Body.js
@@ -0,0 +1,107 @@
+/**
+ * In this file, we create a React component
+ * which incorporates components provided by Material-UI.
+ */
+import React, {Component} from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import {deepOrange500} from 'material-ui/styles/colors';
+import FlatButton from 'material-ui/FlatButton';
+import {List, ListItem} from 'material-ui/List';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import Subheader from 'material-ui/Subheader';
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 20,
+  },
+};
+
+
+class Body extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleRequestClose() {
+    this.setState({
+      open: false,
+    });
+  }
+
+  handleTouchTap() {
+    this.setState({
+      open: true,
+    });
+  }
+
+  render() {
+    const standardActions = (
+        <FlatButton
+            label="Ok"
+            primary={true}
+            onTouchTap={this.handleRequestClose}
+        />
+    );
+
+    return (
+          <div style={styles.container}>
+            <Dialog
+                open={this.state.open}
+                title="Super Secret Password"
+                actions={standardActions}
+                onRequestClose={this.handleRequestClose}
+            >
+              1-2-3-4-5
+            </Dialog>
+            <h1>Material-UI</h1>
+            <h2>example project</h2>
+            <RaisedButton
+                label="Super Secret Password"
+                secondary={true}
+                onTouchTap={this.handleTouchTap}
+            />
+            <List>
+              <Subheader>Nested List Items</Subheader>
+              <ListItem primaryText="投资收益2" leftIcon={<ContentSend />} />
+              <ListItem primaryText="退出账户" leftIcon={<ContentDrafts />} />
+              <ListItem
+                  primaryText="投资"
+                  leftIcon={<ContentInbox />}
+                  initiallyOpen={false}
+                  primaryTogglesNestedList={true}
+                  nestedItems={[
+          <ListItem
+            key={1}
+            primaryText="Starred"
+            leftIcon={<ActionGrade />}
+          />,
+          <ListItem
+            key={2}
+            primaryText="Sent Mail"
+            leftIcon={<ContentSend />}
+            disabled={true}
+            nestedItems={[
+              <ListItem key={1} primaryText="Drafts" leftIcon={<ContentDrafts />} />,
+            ]}
+          />,
+        ]}
+              />
+            </List>
+          </div>
+    );
+  }
+}
+
+export default Body;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,90 @@
-require('normalize.css');
-require('styles/App.css');
-
+/**
+ * In this file, we create a React component
+ * which incorporates components provided by Material-UI.
+ */
 import React from 'react';
+import {deepOrange500} from 'material-ui/styles/colors';
+import AppBar from 'material-ui/AppBar';
 
-let yeomanImage = require('../images/yeoman.png');
+import {Tabs, Tab} from 'material-ui/Tabs';
+import FontIcon from 'material-ui/FontIcon';
+import MapsPersonPin from 'material-ui/svg-icons/maps/person-pin';
 
-class AppComponent extends React.Component {
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
+import User from './User';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import SignOut from './SignOut';
+import {hasLogin,requireAuth,noAuth} from './Auth';
+const styles = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+};
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
+class Main extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.handleActive = this.handleActive.bind(this);
+  }
+  /**
+   * 跳转tab
+   * @param tab
+   */
+  handleActive(tab) {
+    this.context.router.push(tab.props['data-route']);
+  }
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div>
+          <AppBar
+            title="梅 花 盘"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+          {this.props.children}
+          <Tabs>
+            <Tab
+              icon={<FontIcon className="material-icons">monetization_on</FontIcon>}
+              label="行情"
+              data-route="/"
+              onActive={this.handleActive}
+            />
+            <Tab
+              icon={<FontIcon className="material-icons">account_box</FontIcon>}
+              label="账户"
+              data-route="/ucenter"
+              onActive={this.handleActive}
+            />
+            <Tab
+              icon={<FontIcon className="material-icons">explore</FontIcon>}
+              label="数据"
+              data-route="/news"
+              onActive={this.handleActive}
+            />
+            <Tab
+              icon={<FontIcon className="material-icons">help</FontIcon>}
+              label="帮助"
+              data-route="/help"
+              onActive={this.handleActive}
+            />
+          </Tabs>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
-
-AppComponent.defaultProps = {
-};
-
-export default AppComponent;
+Main.contextTypes = {
+  router: React.PropTypes.object.isRequired
+}
+export default Main;
